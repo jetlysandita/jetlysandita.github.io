2946cff1ac63ebdcb72e24c6342f5d04f672e402
jetly sandita <jetly27sandita@gmail.com>
2019-12-19 22:46:15 +0700

    change when load

--- dino2.js
+++ dino2.js
@@ -1,46 +1,49 @@
 //keyboard event
 document.body.onkeydown = function(e){
-  if(e.keyCode === 87 || e.keyCode === 82){
-      dino2.alt = dino2.alt === 'run' ? 'walk' : 'run'
+  if(e.keyCode === 87){
+    dino.alt = 'walk' 
+  }
+  if(e.keyCode === 82){
+    dino.alt = 'run' 
   }
   if(e.keyCode === 32){
-      temp = dino2.alt
-      dino2.alt = 'jump'
+    temp = dino.alt
+    dino.alt = 'jump'
   }
 }
 //change image
 function dino2do(){
-  number2[0] = Number(number2[0])
-  if(dino2.alt==='walk'){
+  number[0] = Number(number[0])
+  if(dino.alt==='walk'){
 
-    if(number2[0] >= 10){
-      number2[0] = 1
+    if(number[0] >= 10){
+      number[0] = 1
     }else{
-      number2[0] = number2[0] + 1
+      number[0] = number[0] + 1
     }
 
-    dino2.src = dino2.alt + '/' + number2[0] + '.png'
+    dino.src = dino.alt + '/' + number[0] + '.png'
 
-  }else if(dino2.alt === 'run'){
+  }else if(dino.alt === 'run'){
 
-    if(number2[0] >=8){
-      number2[0] = 1
+    if(number[0] >=8){
+      number[0] = 1
     }else{
-      number2[0] = number2[0] + 1
+      number[0] = number[0] + 1
     }
 
-    dino2.src = dino2.alt + '/' + number2[0] + '.png'
+    dino.src = dino.alt + '/' + number[0] + '.png'
 
-  }else{
+  }else if(dino.alt==='jump'){
 
-    if(number2[0] >=12){
-      dino2.alt=temp
-      number2[0]=1
+    if(number[0] >=12){
+      dino.alt=temp
+      number[0]=1
     }else{
-      number2[0] = number2[0] + 1
+      number[0] = number[0] + 1
     }
 
-    dino2.src = dino2.alt + '/' + number2[0] + '.png'
+    dino.src = dino.alt + '/' + number[0] + '.png'
 
   }
 }
--- main.js
+++ main.js
@@ -5,13 +5,7 @@ content.src = 'idle/'+'1.png'
 //get number only dino 1 url
 let current = (content.src).split('/')
 let number = (current[current.length-1]).split('.')
-//dino 2
-let dino2 = document.getElementById('dino2')
-//set default
-dino2.src = 'walk/'+'1.png'
-//get number only dino 2 url
-let current2 = (dino2.src).split('/')
-let number2 = (current2[current2.length-1]).split('.')
+
 //default pointer
 let pointer = 'crosshair'
 //default temp
