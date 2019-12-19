d157be6008af19c66d41ef55d40763062c15a3f1
jetly sandita <jetly27sandita@gmail.com>
2019-12-19 22:07:30 +0700

    change code main

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-//dino 1
-let content = document.getElementById('dino')
-//set default
-content.src = 'idle/'+'1.png'
-//get number only dino 1 url
-let current = (content.src).split('/')
-let number = (current[current.length-1]).split('.')
-//dino 2
-let dino2 = document.getElementById('dino2')
-//set default
-dino2.src = 'walk/'+'1.png'
-//get number only dino 2 url
-let current2 = (dino2.src).split('/')
-let number2 = (current2[current2.length-1]).split('.')
-//default pointer
-let pointer = 'crosshair'
-//default temp
-let temp = ''
-//always run for interval millisecond
-setInterval(() => {
+$(document).ready(function(){
     //dino 1
-    dino1do()
-    //dino2
-    dino2do()
-}, 100);
+    let content = document.getElementById('dino')
+    //set default
+    content.src = 'idle/'+'1.png'
+    //get number only dino 1 url
+    let current = (content.src).split('/')
+    let number = (current[current.length-1]).split('.')
+    //dino 2
+    let dino2 = document.getElementById('dino2')
+    //set default
+    dino2.src = 'walk/'+'1.png'
+    //get number only dino 2 url
+    let current2 = (dino2.src).split('/')
+    let number2 = (current2[current2.length-1]).split('.')
+    //default pointer
+    let pointer = 'crosshair'
+    //default temp
+    let temp = ''
+    //always run for interval millisecond
+    setInterval(() => {
+        //dino 1
+        dino1do()
+        //dino2
+        dino2do()
+    }, 100);
+})
     
